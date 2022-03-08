2e41ef10c2014f6761ff25e6e9e19837da832195
toomanystrings <conorforan1@gmail.com>
2022-03-08 14:51:49 +0000

    Beginning new module for GUI

---
+++ Modules/DivisionVoid/DivisionVoid.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Conor on 08/03/2022.
+//
+
+#include "DivisionVoid.h"
---
+++ Modules/DivisionVoid/DivisionVoid.h
@@ -0,0 +1,34 @@
+//
+// Created by Conor on 08/03/2022.
+//
+
+#pragma once
+
+#if 0
+
+BEGIN_JUCE_MODULE_DECLARATION
+
+      ID:               divisionvoid
+      vendor:           DivisionVoid
+      version:          0.0.1
+      name:             divisionvoid
+      description:      divisionvoid
+      license:          GPL/Commercial
+      dependencies:     juce_audio_processors, juce_core, juce_events
+                        juce_gui_basics
+
+     END_JUCE_MODULE_DECLARATION
+
+#endif
+
+// JUCE Includes
+#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_core/juce_core.h>
+#include <juce_events/juce_events.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+
+// Binary files to include.
+#include <SharedBinaryResources.h>
+
+// We will need to include files from source here
